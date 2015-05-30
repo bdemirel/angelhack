979b4b07a75b0d4b063a52e2be7eac8794cbec63
bdemirel <sulhdemirel@hotmail.com>
2015-05-30 18:18:48 +0300

    register user

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var express = require('express'),
     app = express();
 
 app.use('/auth/sign', require('.routes/login.js'));
+app.use('/auth/create', require('.routes/add_user.js'));
 
 app.listen('3000');
---
+++ routes/add_user.js
@@ -0,0 +1,66 @@
+var route = require('express').Router(),
+    mongo = require('mongoskin'),
+    db = mongo.db('mongodb://130.211.104.226:27017/nts'),
+    jwt = require('jsonwebtoken'),
+    bodyParser = require('body-parser');
+
+route.use(bodyParser.json());
+route.post('/', function(req, res)
+{
+  var fname = req.body.firstname,
+      lname = req.body.lastname,
+      uname = req.body.username,
+      pword = req.body.password,
+      utype = req.body.email ? 1 : 0,
+      email = (utype == 1) ? req.body.email : undefined;
+
+  db.users.find({'uname':uname}).toArray(function(err, user)
+  {
+    if (err)
+    {
+      res.status(403).json({'err':'Error in db query!'});
+    }
+
+    if (user.fname)
+    {
+      res.status(400).json({'err':'Username exists!'});
+    }
+
+    db.users.insert({'fname':fname, 'lname':lname, 'uname':uname, 'utype':utype, 'email':email}, {'w':1},       function(err, doc)
+    {
+      if (err)
+      {
+        res.status(403).json({'err':'Error in db insertion!'});
+      }
+
+      var uid = doc._id,
+          salt = uid.slice(0,3)+username.slice(3,5)+uid.slice(5,10);
+
+      crypto.pbkdf2(pword, salt, 256, 128, 'sha256', function(err, hash)
+      {
+        if (err)
+        {
+          res.status(403).json({'err':'Error in password hashing!'});
+        }
+        db.users.update({'uname':uname}, {$set:{'pword':hash}}, {'w':1}, function(err, resu)
+        {
+          if (err)
+          {
+            db.users.remove({'uname':uname}, {'w':1}, function(err, no)
+            {
+              if (err)
+              {
+                res.status(403).json({'err':'Error in db, couldn not delete!'});
+              }
+
+              res.status(403).json({'err':'Error in db insertion!'});
+            });
+          }
+
+          var token = jwt.sign({'username':uname, 'usertype':utype}, uid, {'expiresInMinutes':30});
+          res.status(201).json({'token':token});
+        });
+      }
+    });
+  });
+});
