f3e4d644872cebe44e0efcb5799c9699213620c3
bdemirel <sulhdemirel@hotmail.com>
2015-05-30 18:50:54 +0300

    correct the requires

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express();
 
-app.use('/auth/sign', require('.routes/login.js'));
-app.use('/auth/create', require('.routes/add_user.js'));
+app.use('/auth/sign', require('./routes/login.js'));
+app.use('/auth/create', require('./routes/add_user.js'));
 
 app.listen('3000');
