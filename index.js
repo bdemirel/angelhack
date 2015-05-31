1da008e5d157ca771f7864b1b1ed25d4d46f912a
bdemirel <sulhdemirel@hotmail.com>
2015-05-31 10:19:28 +0300

    clear code

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var express = require('express'),
 app.use('/auth/sign', require('./routes/login.js'));
 app.use('/auth/create', require('./routes/add_user.js'));
 app.use('/event/create', require('./routes/add_event.js'));
-//app.use('/event/list', require('./routes/list_events.js'));
+app.use('/event/list', require('./routes/list_events.js'));
 app.use('/note/create', require('./routes/create_note.js'));
 app.use('/note/list', require('./routes/list_note.js'));
 app.use('/event/details', require('./routes/event_details.js'));
-app.use('/debug', require('./routes/debug.js'));
 
 app.listen('3000');
