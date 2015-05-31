65d2a8ffc48d3d7e53f96653a2b7cdfbf9646d6e
bdemirel <sulhdemirel@hotmail.com>
2015-05-31 08:57:47 +0300

    omment out

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express'),
 app.use('/auth/sign', require('./routes/login.js'));
 app.use('/auth/create', require('./routes/add_user.js'));
 app.use('/event/create', require('./routes/add_event.js'));
-app.use('/event/list', require('./routes/list_events.js'));
+//app.use('/event/list', require('./routes/list_events.js'));
 app.use('/note/create', require('./routes/create_note.js'));
 app.use('/note/list', require('./routes/list_note.js'));
 app.use('/event/details', require('./routes/event_details.js'));
