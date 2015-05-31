230a65ba0ec7733bbb8d44367766e0ea9a12028c
bdemirel <sulhdemirel@hotmail.com>
2015-05-31 08:58:43 +0300

    erro

--- routes/debug.js
+++ routes/debug.js
@@ -3,10 +3,10 @@ Server = mongo.Server,
     Db = mongo.Db;
 
 var db = new Db('nts', new Server('130.211.104.226', 27017));
-db.open(function(err, db))
+db.open(function(err, db)
 {
   if (err)
   {
     console.log(err);
   }
-}
+});
